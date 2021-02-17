0c2d745c509de5b6a41e7ab481a3b7840e030a9c
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-02-17 10:18:47 -0700

    updated contact, work, and css

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+let EmailElement = document.querySelector('#Email');
+console.log(EmailElement);
+let messageElement = document.querySelector("#message");
+
+let submitbutton = document.querySelector('#submit-button');
+submitbutton.addEventListener('click', function(e) {
+     e.preventDefault();
+     
+    let emailValue = EmailElement.value;
+    let messageValue = messageElement.value;
+    if(emailValue.includes('@')) {
+    	//all good
+    	alert('Thank you For your message!')
+    } else{
+    	alert('oops, please enter an email address!')}
+})
